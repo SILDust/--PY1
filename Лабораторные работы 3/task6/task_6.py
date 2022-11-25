f16ad4c16c749f0e55b0838c6cf4f9d8e4a23e6e
Kirill_Sklyarov <118133793+SILDust@users.noreply.github.com>
2022-11-25 23:18:51 +0300

    Update task_6.py

--- Лабораторные работы 3/task6/task_6.py
+++ Лабораторные работы 3/task6/task_6.py
@@ -4,10 +4,8 @@ max_ = 0
 prob = 0
 for id, val in enumerate (list_numbers):
     if val >= max_:
-        max_ = val
-        max_id = id
+        max_, max_id = val, id
 
-list_numbers[max_id] = list_numbers[-1]
-list_numbers[-1] = max_
+list_numbers[max_id], list_numbers[-1] = list_numbers[-1], max_
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
 
