f95e450d053bcb5a8b3b852e483dd6759b8df6df
Kirill_Sklyarov <118133793+SILDust@users.noreply.github.com>
2022-12-09 21:21:34 +0300

    Обновленное_2.0_task_3

--- Лабораторная работа №5/task3/task_3.py
+++ Лабораторная работа №5/task3/task_3.py
@@ -1,10 +1,10 @@
-from random import randint
+from random import sample
 
 
-def get_unique_list_numbers(a) -> list[int]:
-    return [randint(-10, 10) for _ in range(a)]
+def get_unique_list_numbers(a, b, c) -> list[int]:
+    return [i for i in sample(range(b, c+1), a)]
 
 
-list_unique_numbers = get_unique_list_numbers(15)
+list_unique_numbers = get_unique_list_numbers(15, -10, 10)
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
