c73882fa70a532eee7a4e2ef1f0ce4dfe068a17f
Kirill_Sklyarov <118133793+SILDust@users.noreply.github.com>
2022-12-09 13:52:29 +0300

    Обновленное task_5

--- Лабораторная работа №5/task5/task_5.py
+++ Лабораторная работа №5/task5/task_5.py
@@ -1,9 +1,12 @@
+import random
+import string
+
+
 def get_random_password(n) -> str:
-    import random
-    all_list = [chr(random.randint(ord('a'), ord('z'))) for i in range(int(n))] +\
-               [chr(random.randint(ord('A'), ord('Z'))) for i in range(int(n))] +\
-               [random.randint(0, 9) for i in range(int(n))]
-    password_list = random.sample(all_list, int(n))
-    return "".join(map(str, password_list))
+    all_list = [letter for letter in (string.ascii_lowercase + string.ascii_uppercase)] + \
+               [str(digit) for digit in range(10)]
+    password_list = random.sample(all_list, n)
+    return "".join(password_list)
+
 
 print(get_random_password(8))
