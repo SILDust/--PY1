d4f014eb032a9eeeb33ab5e972c3ec2d3200a478
Kirill_Sklyarov <118133793+SILDust@users.noreply.github.com>
2023-01-22 16:08:21 +0300

    Обновленная Лаб.работа №1

--- Лабораторная работа 1.0/task1/task_1.py
+++ Лабораторная работа 1.0/task1/task_1.py
@@ -1,8 +1,9 @@
 import doctest
+from typing import Union
 
 
 class Tables:
-    def __init__(self, material: str, weight: float):
+    def __init__(self, material: str, weight: Union[int, float]):
         if not isinstance(material, str):
             raise TypeError("Материал стола должен описан словесно")
         if not weight > 0:
@@ -11,11 +12,23 @@ class Tables:
         self.material = material
         self.weight = weight
 
-    def material_description(self):
-        print(f"Материал стола - {self.material}")
+    def material_description(self, material: str) -> str:
+        """
+        :return: Материал стола
+        Примеры:
+        >>> wood_table = Tables("Дерево", 10)
+        >>> wood_table.material_description
+        """
+        return f"Материал стола - {material}"
 
-    def weight_description(self):
-        print(f"Вес стола - {self.weight}")
+    def weight_description(self, weight: Union[int, float]) -> str:
+        """
+        :return: Вес стола
+        Примеры:
+        >>> wood_table = Tables("Дерево", 10)
+        >>> wood_table.weight_description
+        """
+        return f"Вес стола - {weight}"
 
 
 class Books:
@@ -31,30 +44,48 @@ class Books:
         self.page_number = page_number
         self.genre = genre
 
-    def author(self) -> str:
+    def author(self, author: str) -> str:
         # Даётся краткая сводка по автору книги
+        """
+        :return: Краткая сводка по автору книги
+        Примеры:
+        >>> Idiot = Books("Достоевский", 640, "Роман")
+        >>> Idiot.author
+        """
         ...
 
-    def page_number(self) -> int:
+    def page_number(self, page_number: int) -> int:
         # Выводится число страниц в данной книге
+        """
+        :return: Число страниц в данной книге
+        Примеры:
+        >>> Idiot = Books("Достоевский", 640, "Роман")
+        >>> Idiot.page_number
+        """
         ...
 
-    def genre(self) -> str:
+    def genre(self, genre: str) -> str:
         # Даётся краткая сводка по жанру книги
+        """
+        :return: Краткая сводка по жанру книги
+        Примеры:
+        >>> Idiot = Books("Достоевский", 640, "Роман")
+        >>> Idiot.genre
+        """
         ...
 
 
 class consumption_indicator:
-    def __init__(self, distance, available_fuel, consumption_per_km: float):
-        if not isinstance(distance, float):
+    def __init__(self, distance: Union[int, float], available_fuel: Union[int, float], consumption_per_km: Union[int, float]):
+        if not isinstance(distance, (int, float)):
             raise TypeError("Расстояние должно указываться числом")
         if not distance > 0:
             raise ValueError("Расстояние должно быть указано положительным числом")
-        if not isinstance(available_fuel, float):
+        if not isinstance(available_fuel, (int, float)):
             raise TypeError("Доступный объем топлива должен указываться числом")
         if not available_fuel > 0:
             raise ValueError("Доступный объем топлива должен быть указан положительным числом")
-        if not isinstance(consumption_per_km, float):
+        if not isinstance(consumption_per_km, (int, float)):
             raise TypeError("Расход на 100 км должен указываться числом")
         if not consumption_per_km > 0:
             raise ValueError("Расход на 100 км должен быть указан положительным числом")
@@ -63,14 +94,28 @@ class consumption_indicator:
         self.available_fuel = available_fuel
         self.consumption_per_km = consumption_per_km
 
-    def required_fuel_for_route(self) -> int:
+    def required_fuel_for_route(self, consumption_per_km: Union[int, float], distance: Union[int, float]) -> (int, float):
         # Вычисляется необходимое количество топливо для построенного маршрута с учетом расхода автомобиля
+        """
+        :required_fuel: необходимое кол-во топливо на маршрут с учетом расхода топлива автомобилем
+        :return: Выдает значение требуемого значения топлива на маршрут с учетом расхода топлива автомобилем
+        Примеры:
+        >>> Car = consumption_indicator(100, 81, 9.5)
+        >>> Car.required_fuel_for_route
+        """
         ...
 
-    def comparing_necessary_and_available_fuel(self) -> bool:
+    def comparing_necessary_and_available_fuel(self, available_fuel: Union[int, float]) -> bool:
         # Выдается информация о том, хватает ли текущего запаса на заданную дистанцию.
+        """
+        :return: Выдает информацию хватит ли имеющегося топлива на совершаемый маршрут
+        Примеры:
+        >>> Car = consumption_indicator("Тест", 81, 9.5)
+        >>> Car.comparing_necessary_and_available_fuel
+        """
         ...
 
 
 if __name__ == "__main__":
     doctest.testmod()
+    
